04cd27f890b335945c24739cedb58010893050b1
Collin Fogerty <clf9@njit.edu>
2022-05-04 14:45:40 -0400

    minor mistakes corrected

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -32,4 +32,4 @@ def create_upload_folder():
     # make a directory if it doesn't exist
     if not os.path.exists(uploadfolder):
         os.mkdir(uploadfolder)
-    db.create_all()te_all()
+    db.create_all()
